5f56c6864055b071e18cf38309fbab8837d6fe96
Mayanja Justus <justusmayanja7@gmail.com>
2026-06-15 15:46:47 +0300

    Local and global variable exercise

---
+++ Local_And_Global_Variable.py
@@ -0,0 +1,14 @@
+# Global variable
+name = "John"
+
+def show_info():
+
+    # Local variable
+    age = 20
+
+    print("Name:", name)
+    print("Age:", age)
+
+show_info()
+
+print("Name outside function:", name)
---
+++ Worldcup.py
@@ -0,0 +1,14 @@
+def display_student_info():
+
+    name = input("Enter student name: ")
+    age = input("Enter student age: ")
+    course = input("Enter course: ")
+    student_number = input("Enter student number: ")
+
+    print("\n===== STUDENT INFORMATION =====")
+    print("Name:", name)
+    print("Age:", age)
+    print("Course:", course)
+    print("Student Number:", student_number)
+
+display_student_info()
