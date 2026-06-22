3cb01cc5a74c3fc6e24d468fbe5e0a98bd708b64
Mayanja Justus <justusmayanja7@gmail.com>
2026-06-22 11:00:56 +0300

    exercise on classes and constructors

---
+++ 3Restuarant.py
@@ -0,0 +1,18 @@
+class Restaurant:
+
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print("\nRestaurant Name:", self.restaurant_name)
+        print("Cuisine Type:", self.cuisine_type)
+
+
+restaurant1 = Restaurant("Sunrise Restaurant", "African Food")
+restaurant2 = Restaurant("Pizza Palace", "Italian Food")
+restaurant3 = Restaurant("Dragon Kitchen", "Chinese Food")
+
+restaurant1.describe_restaurant()
+restaurant2.describe_restaurant()
+restaurant3.describe_restaurant()
